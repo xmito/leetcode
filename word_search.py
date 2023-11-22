a84844523dcef47a0dd5e7da8ff86388805fcce0
Matej Hulín <mito.hulin@gmail.com>
2023-11-22 23:18:15 +0100

    word search constraints

--- word_search.py
+++ word_search.py
@@ -5,6 +5,12 @@ from typing import List
 # in the grid. The word can be constructed from letters of sequentially adjacent cells,
 # where adjacent cells are horizontally or vertically neighboring. The same letter cell
 # may not be used more than once.
+# Constraints:
+# m == board.length
+# n = board[i].length
+# 1 <= m, n <= 6
+# 1 <= word.length <= 15
+# board and word consists of only lowercase and uppercase English letters.
 
 # Time complexity: O(n * 3ˆl) - where n is number of cells and l is length of word. dfs
 # function is called at most once for each board cell and from each cell during traversal,
