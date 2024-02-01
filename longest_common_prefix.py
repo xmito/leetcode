2fcdb247b17b8f2d43b4c9b852ddd1655dac46ba
Matej Hulín <mito.hulin@gmail.com>
2024-02-01 14:44:16 +0100

    longest common prefix change

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -7,6 +7,7 @@ class TrieNode:
         self.children = {}
 
 
+# Longest Common Prefix (Easy)
 # Write a function to find the longest common prefix string amongst an array
 # of strings. If there is no common prefix, return an empty string "".
 # Constraints:
